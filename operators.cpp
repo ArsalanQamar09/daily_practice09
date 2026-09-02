e07f8ed3bc83aa572ee6882bedb49dcb176fc762
Arsalan Qamar <arsalanqamar2006@gmail.com>
2026-09-02 21:35:48 +0530

    02/09

---
+++ operators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool exp1=true ;
+    bool exp2=true;
+    cout<<(exp1&&exp2)<<endl;
+    cout<<(exp1||exp2)<<endl;
+    cout<<!exp1<<endl;
+    int a=10;
+    cout<<a++<<endl;//10
+    int b=5;
+    cout<<--b<<endl;//4
+    int c=a+4;
+    cout<<c<<endl;//15
+    return 0;
+
+
+
+}
