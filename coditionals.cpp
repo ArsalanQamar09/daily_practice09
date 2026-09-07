d65cf508239ecb825f0041756cd40c5931fcc487
Arsalan Qamar <arsalanqamar2006@gmail.com>
2026-09-07 15:57:40 +0530

    day 3

---
+++ coditionals.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int num;
+    // cin>>num;
+    // if (num%2==0){
+    //     cout<<"even"<<endl;
+    // }
+    // else if(num<0){
+    //     cout<<"not defined"<<endl;//only check else if condition 
+    // }
+    // else{
+    //     cout<<"odd"<<endl;
+    // }
+    // return 0;
+    // int age ;
+    // cin >>age;
+    // if (age<=18){
+    //     cout<<"child"<<endl;
+    // }
+    //     else if(age>18 && age <=60){
+    //         cout<<"adult"<<endl;
+    //     }
+    //     else if(age>60 && age <=90){
+    //         cout<<"senior citizen"<<endl;
+    //     }
+    //     else {
+    //         cout<<"daed"<<endl;
+    //     }
+    //     return 0;
+    // int age;
+    // cin >>age ;
+    // age <18 ? cout<<"not right to vote"<<endl :cout<<"right to vote "<<endl;//ternary operator
+    // return 0;
+    // int num;
+    // cin >>num;
+    // if ((num%2==0)&&(num%3==0)){//logical and operator
+    //     cout<<num<<endl;
+    //     return 0;
+    int num;
+    cin >>num;
+    if ((num%2==0)||(num%3==0)){//logical or operator
+        cout<<num<<endl;
+        return 0;
+    }
+
+
+    
+    
+}
